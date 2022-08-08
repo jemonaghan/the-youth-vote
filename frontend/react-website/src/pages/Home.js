b1dfb5581a605af1dd66199dd5833080fa0445e5
sckojj <jemillascotten@gmail.com>
2022-08-08 20:48:44 +0100

    homepage edit

--- frontend/react-website/src/pages/Home.js
+++ frontend/react-website/src/pages/Home.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import Banner from '../components/Banner.js';
-import '../../src/App.css'
-  
-const Home = () => {
-  return (
-    <div class="home-page">
+import { Link } from "react-router-dom";
 
-      <Banner />
-      
+import BlueButton from '../components/buttons/BlueButton';
+import OrangeButton from '../components/buttons/OrangeButton';
 
-      </div>
-  );
+import "./Home.css";
+
+function Home() {
+	return (
+		<div id="homepage">
+			<div id="heading">
+				<h1>THE YOUTH VOTE</h1>
+			</div>
+
+			<div class="text-box">
+				<h2>Youth Voting Matters</h2>
+				<p>Voting is habit-forming:
+When young people understand the voting process and vote, they are more likely to do so when they are older.  Getting people to vote early could be the key to raising a 
+new generation of voters. </p>
+			</div>
+
+			<div id="vote">
+				<Link to="/vote"><OrangeButton buttonLabel="VOTE!" /></Link>
+			</div>
+
+			<div class="text-box">
+				<h2>Sign Up To Take Part</h2>
+				<p>Information for teaches/educators. Text to explain how the sign up process works, poll cards etc </p>
+			</div>
+
+			<div id="sign-up">
+				<Link to="/sign-up"><BlueButton buttonLabel="SIGN UP" /></Link>
+			</div>
+			
+		</div>
+	);
 }
-  
+
 export default Home;
