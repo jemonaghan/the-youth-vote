a95494770ec0e121f1908ce52ba69d37f02d5b79
sckojj <jemillascotten@gmail.com>
2022-08-11 22:54:13 +0100

    styling

--- frontend/react-website/src/components/Footer.js
+++ frontend/react-website/src/components/Footer.js
@@ -8,30 +8,23 @@ function Footer() {
   
   return (
    
-    <footer className='footer--pin' role ='navigation'>
-      <div className='footer'>
-        <div className='row text-end'>
-          <div className='col'>
-          </div>
-             <div className='col-1 text-center'>
-              <a className='foot-link'href='https://twitter.com/CodeFirstGirls?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'>
-               <FaTwitter color='white' size='1.3em' /></a>
-             </div>
-               <div className='row'>
-                <div className='col-sm-8 col-md-4 text-start'>
-                <Link to='/'className='foot-sign'>Sign up</Link>
-                </div>
-             <div className='col-4 d-none d-md-block text-center'>
-                <Link to='/'className='foot-link'>Home</Link>
-               </div>
-             <div className='col-4 text-end'>
-                <Link to='javascript:void(0)'
-               className='foot-link'
-               onClick={() => window.location = 'mailto:the-youth-vote@domain.com'}>Contact</Link>
-            </div>
-          </div>
+    <footer className="footer" role ='navigation'>
+
+        <div className='footer-item'>
+            <Link to='/sign-up'className='foot-link'>Sign up</Link>
         </div>
-      </div>
+
+        <div className='footer-item'>
+            <Link to='/'className='foot-link'>The Youth Vote</Link>
+        </div>
+             
+        <div className='footer-item two-stack'>
+            <a className='foot-link'href='https://twitter.com/CodeFirstGirls?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'>
+            <FaTwitter color='white' size='1.3em' /></a>
+            <a className='foot-link' href="mailto:the-youth-vote@domain.com">Contact</a>
+        </div>
+
+
     </footer>
   )
 }
--- frontend/react-website/src/components/NavLinks.js
+++ frontend/react-website/src/components/NavLinks.js
@@ -5,7 +5,7 @@ const NavLinks = (props) => {
     return (
         <ul>
             <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-                <a href="/" className="navbar-logo">The Youth Vote</a>
+                <a href="/" className="navbar-link">The Youth Vote</a>
             </li>
             <li onClick={() => props.isMobile && props.closeMobileMenu()}>
                 <a href="/vote" className="nav-link">Vote</a>
@@ -14,7 +14,7 @@ const NavLinks = (props) => {
                 <a href="/results" className="nav-link">Results</a>
             </li>
             <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-                <a href="/sign-up" className="nav-link">Sign Up</a>
+                <a href="/learn-more" className="nav-link">Learn More</a>
             </li>
        </ul>
     );
